de3a3003500489b78874c54041050d9a4b1cc3a5
Mohamed Dahani <mdahani@e2r11p11.1337.ma>
2024-12-26 16:32:39 +0100

    create the data

--- src/app/components/works.jsx
+++ src/app/components/works.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Img from "../../../public/heronfinance.webp";
+import data from "../../db/data";
 
 const Works = () => {
   return (
@@ -9,99 +10,41 @@ const Works = () => {
         Featured work
       </h1>
       <div className="grid grid-cols-1 place-items-center gap-10 lg:grid-cols-4">
-        <div className="flex items-center gap-5">
-          <div className="grid h-full w-full overflow-hidden rounded-md bg-slate-200 backdrop-blur-md dark:bg-white/10">
-            <div className="p-4">
-              <h3 className="text-lg font-semibold text-black dark:text-white">
-                Heron Finance
-              </h3>
+        {data.slice(0, 3).map((item, index) => {
+          return (
+            <div className="flex items-center gap-5" key={index}>
+              <div className="grid h-full w-full overflow-hidden rounded-md bg-slate-200 backdrop-blur-md dark:bg-white/10">
+                <div className="p-4">
+                  <h3 className="text-lg font-semibold text-black dark:text-white">
+                    {item.title}
+                  </h3>
+                </div>
+                <div className="relative h-[300px] w-full">
+                  <Image
+                    alt="Heron Finance"
+                    src={item.img}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="left top"
+                    loading="lazy"
+                    decoding="async"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </div>
+                <div className="bg-[#e1e8f0] dark:bg-[#303032] px-4 py-6">
+                  <p className="text-black dark:text-white text-sm">
+                    {item.description}
+                  </p>
+                </div>
+                <div className="h-20 bg-slate-300 backdrop-blur-md dark:bg-white/5 flex justify-center items-center">
+                  <button className="text-white bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 py-2 px-14 font-bold rounded-full hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 transition-colors">
+                    <Link href={item.link}>Visit</Link>
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="relative h-[300px] w-full">
-              <Image
-                alt="Heron Finance"
-                src={Img}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="left top"
-                loading="lazy"
-                decoding="async"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </div>
-            <div className="bg-[#e1e8f0] dark:bg-[#303032] px-4 py-6">
-              <p className="text-black dark:text-white text-sm">
-                The returns of stocks, the consistency of bonds.
-              </p>
-            </div>
-            <div className="h-20 bg-slate-300 backdrop-blur-md dark:bg-white/5 flex justify-center items-center">
-              <button className="text-white bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 py-2 px-14 font-bold rounded-full hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 transition-colors">
-                <Link href="">Visit</Link>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center gap-5">
-          <div className="grid h-full w-full overflow-hidden rounded-md bg-slate-200 backdrop-blur-md dark:bg-white/10">
-            <div className="p-4">
-              <h3 className="text-lg font-semibold text-black dark:text-white">
-                Heron Finance
-              </h3>
-            </div>
-            <div className="relative h-[300px] w-full">
-              <Image
-                alt="Heron Finance"
-                src={Img}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="left top"
-                loading="lazy"
-                decoding="async"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </div>
-            <div className="bg-[#e1e8f0] dark:bg-[#303032] px-4 py-6">
-              <p className="text-black dark:text-white text-sm">
-                The returns of stocks, the consistency of bonds.
-              </p>
-            </div>
-            <div className="h-20 bg-slate-300 backdrop-blur-md dark:bg-white/5 flex justify-center items-center">
-              <button className="text-white bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 py-2 px-14 font-bold rounded-full hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 transition-colors">
-                <Link href="">Visit</Link>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center gap-5">
-          <div className="grid h-full w-full overflow-hidden rounded-md bg-slate-200 backdrop-blur-md dark:bg-white/10">
-            <div className="p-4">
-              <h3 className="text-lg font-semibold text-black dark:text-white">
-                Heron Finance
-              </h3>
-            </div>
-            <div className="relative h-[300px] w-full">
-              <Image
-                alt="Heron Finance"
-                src={Img}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="left top"
-                loading="lazy"
-                decoding="async"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </div>
-            <div className="bg-[#e1e8f0] dark:bg-[#303032] px-4 py-6">
-              <p className="text-black dark:text-white text-sm">
-                The returns of stocks, the consistency of bonds.
-              </p>
-            </div>
-            <div className="h-20 bg-slate-300 backdrop-blur-md dark:bg-white/5 flex justify-center items-center">
-              <button className="text-white bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 py-2 px-14 font-bold rounded-full hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 transition-colors">
-                <Link href="">Visit</Link>
-              </button>
-            </div>
-          </div>
-        </div>
+          );
+        })}
         <button className="text-white bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 py-2 px-5 font-bold rounded-full hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 transition-colors">
           <Link href="">More projects</Link>
         </button>
