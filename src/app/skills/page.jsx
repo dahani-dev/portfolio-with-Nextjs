72214339e716c9db4ad7a823db16644f231c9497
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-03-16 21:51:48 +0000

    add a swiper in skills

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   FaHtml5,
   FaCss3Alt,
@@ -19,72 +21,30 @@ import {
 } from "react-icons/si";
 import { TbBrandReactNative } from "react-icons/tb";
 
+// Import Swiper modules
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules"; // Import Swiper Autoplay module
+import "swiper/css"; // Swiper core styles
+
 const Skills = () => {
   const skills = [
-    {
-      icon: <FaHtml5 className="cursor-pointer text-[#e34f28]" />,
-      label: "HTML5",
-    },
-    {
-      icon: <FaCss3Alt className="cursor-pointer text-[#1572b7]" />,
-      label: "CSS3",
-    },
-    {
-      icon: <FaJsSquare className="cursor-pointer text-[#f7df1d]" />,
-      label: "JavaScript",
-    },
-    {
-      icon: <FaReact className="cursor-pointer text-[#62dafc]" />,
-      label: "React.js",
-    },
-    {
-      icon: (
-        <RiNextjsFill className="cursor-pointer text-black dark:text-white" />
-      ),
-      label: "Next.js",
-    },
-    {
-      icon: <FaBootstrap className="cursor-pointer text-[#7211f4]" />,
-      label: "Bootstrap",
-    },
-    {
-      icon: <RiTailwindCssFill className="cursor-pointer text-[#37b2ad]" />,
-      label: "Tailwind CSS",
-    },
-    {
-      icon: <SiMui className="cursor-pointer text-[#0073e6]" />,
-      label: "Material UI",
-    },
-    {
-      icon: <FaNode className="cursor-pointer text-[#339933]" />,
-      label: "Node.js",
-    },
-    {
-      icon: <SiExpress className="cursor-pointer text-black dark:text-white" />,
-      label: "Express.js",
-    },
-    {
-      icon: <SiMongodb className="cursor-pointer text-[#48a248]" />,
-      label: "MongoDB",
-    },
-    {
-      icon: <SiMysql className="cursor-pointer text-[#48789d]" />,
-      label: "MySQL",
-    },
-    { icon: <SiPhp className="cursor-pointer text-[#767bb3]" />, label: "PHP" },
-    {
-      icon: <SiLaravel className="cursor-pointer text-[#ff2d20]" />,
-      label: "Laravel",
-    },
-    {
-      icon: <SiTypescript className="cursor-pointer text-[#007acd]" />,
-      label: "TypeScript",
-    },
-    {
-      icon: <TbBrandReactNative className="cursor-pointer text-[#57c4db]" />,
-      label: "React Native",
-    },
-    { icon: <SiGit className="cursor-pointer text-[#ef5032]" />, label: "Git" },
+    { icon: <FaHtml5 className="text-[#e34f28]" /> },
+    { icon: <FaCss3Alt className="text-[#1572b7]" /> },
+    { icon: <FaJsSquare className="text-[#f7df1d]" /> },
+    { icon: <FaReact className="text-[#62dafc]" /> },
+    { icon: <RiNextjsFill className="text-black dark:text-white" /> },
+    { icon: <FaBootstrap className="text-[#7211f4]" /> },
+    { icon: <RiTailwindCssFill className="text-[#37b2ad]" /> },
+    { icon: <SiMui className="text-[#0073e6]" /> },
+    { icon: <FaNode className="text-[#339933]" /> },
+    { icon: <SiExpress className="text-black dark:text-white" /> },
+    { icon: <SiMongodb className="text-[#48a248]" /> },
+    { icon: <SiMysql className="text-[#48789d]" /> },
+    { icon: <SiPhp className="text-[#767bb3]" /> },
+    { icon: <SiLaravel className="text-[#ff2d20]" /> },
+    { icon: <SiTypescript className="text-[#007acd]" /> },
+    { icon: <TbBrandReactNative className="text-[#57c4db]" /> },
+    { icon: <SiGit className="text-[#ef5032]" /> },
   ];
 
   return (
@@ -92,23 +52,57 @@ const Skills = () => {
       <section className="container mx-auto py-32 px-6">
         <h1 className="text-5xl font-semibold text-white">My Skills</h1>
       </section>
-      <section className="bg-slate-200 dark:bg-zinc-900 py-20 px-6">
-        <div className="container mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-10 place-items-center">
-          {skills.map(({ icon, label }, index) => (
-            <div
-              key={index}
-              aria-label={label}
-              className="flex flex-col items-center group"
-            >
-              <div className="text-7xl md:text-9xl transition-transform group-hover:scale-110">
-                {icon}
-              </div>
-              <p className="mt-2 text-lg text-[#384152] dark:text-[#d1d4db]">
-                {label}
-              </p>
-            </div>
+
+      <section className="bg-slate-200 dark:bg-zinc-900 py-20 px-6 flex gap-20 flex-col">
+        <Swiper
+          modules={[Autoplay]} // Add Autoplay module
+          spaceBetween={30}
+          loop={true}
+          speed={6000} // Adjust speed for smooth scrolling
+          slidesPerView={3} // Default slides per view (mobile)
+          freeMode={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false, // Ensures continuous autoplay
+          }}
+          breakpoints={{
+            640: { slidesPerView: 4 }, // On small screens
+            768: { slidesPerView: 5 }, // On tablets
+            1024: { slidesPerView: 6 }, // On desktops
+          }}
+          className="container mx-auto"
+        >
+          {skills.map(({ icon }, index) => (
+            <SwiperSlide key={index} className="flex flex-col items-center">
+              <div className="text-7xl md:text-9xl">{icon}</div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
+        <Swiper
+          modules={[Autoplay]} // Add Autoplay module
+          spaceBetween={30}
+          dir="rtl"
+          loop={true}
+          speed={6000} // Adjust speed for smooth scrolling
+          slidesPerView={3} // Default slides per view (mobile)
+          freeMode={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false, // Ensures continuous autoplay
+          }}
+          breakpoints={{
+            640: { slidesPerView: 4 }, // On small screens
+            768: { slidesPerView: 5 }, // On tablets
+            1024: { slidesPerView: 6 }, // On desktops
+          }}
+          className="container mx-auto"
+        >
+          {skills.reverse().map(({ icon }, index) => (
+            <SwiperSlide key={index} className="flex flex-col items-center">
+              <div className="text-7xl md:text-9xl">{icon}</div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </section>
     </article>
   );
