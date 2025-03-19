e925dd334c0cf9bb87bc00bd44105821dd3cdee2
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-03-19 16:41:27 +0000

    finish section of tags

--- src/app/tags/page.jsx
+++ src/app/tags/page.jsx
@@ -1,18 +1,35 @@
+import Image from "next/image";
+import tags from "../../db/tags";
+
 const Tags = () => {
   return (
     <article>
-    <section className="container mx-auto py-40">
-      <h1 className="mr-4 text-5xl font-semibold text-white max-md:pl-6">
-        Tags
-      </h1>
-    </section>
-    <section className="bg-slate-200 dark:bg-zinc-900 py-20 max-md:px-6">
-      <div className="container mx-auto flex items-center justify-between gap-y-40 gap-10 flex-wrap max-sm:justify-center">
-        
-      </div>
-    </section>
-  </article>
-  )
-}
+      <section className="container mx-auto py-32 px-6">
+        <h1 className="mr-4 text-5xl font-semibold text-white max-md:pl-6">
+          Tags
+        </h1>
+      </section>
+      <section className="bg-slate-200 dark:bg-zinc-900 py-20 max-md:px-6">
+        <div className="container mx-auto px-6 relative -top-40 grid grid-cols-1 gap-y-6 md:grid-cols-4 md:gap-x-6">
+          {tags.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="p-6 rounded-2xl transition-all duration-500 hover:bg-black/10 hover:backdrop-blur-sm dark:hover:bg-white/10"
+              >
+                <Image
+                  className="rounded-2xl relative h-[400px] w-full overflow-hidden object-cover"
+                  src={item.img}
+                  alt={item.title}
+                />
+                <h1 className="pt-6 text-xl font-semibold text-black dark:text-white">{item.title}</h1>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </article>
+  );
+};
 
-export default Tags
+export default Tags;
---
+++ src/db/tags.js
@@ -0,0 +1,50 @@
+import career from "../../public/tags/career-dark.webp";
+import webDevlopment from "../../public/tags/web.webp";
+import ai from "../../public/tags/ai.webp";
+import javaScript from "../../public/tags/javascript.webp";
+import freelancing from "../../public/tags/freelance.webp";
+import web3 from "../../public/tags/web3.webp";
+import travel from "../../public/tags/travel.webp";
+import mobileDevelopement from "../../public/tags/mobile.webp";
+import css from "../../public/tags/css.webp";
+
+const tags = [
+  {
+    img: career,
+    title: "Career",
+  },
+  {
+    img: webDevlopment,
+    title: "Web Development",
+  },
+  {
+    img: ai,
+    title: "Ai",
+  },
+  {
+    img: javaScript,
+    title: "JavaScript",
+  },
+  {
+    img: freelancing,
+    title: "Freelancing",
+  },
+  {
+    img: web3,
+    title: "Web 3.0",
+  },
+  {
+    img: travel,
+    title: "Travel",
+  },
+  {
+    img: mobileDevelopement,
+    title: "Mobile Development",
+  },
+  {
+    img: css,
+    title: "CSS",
+  },
+];
+
+export default tags;
